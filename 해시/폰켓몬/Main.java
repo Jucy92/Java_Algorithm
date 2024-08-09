5691ba76e42c92fda50b9bc86acb0a5e9ce7fba4
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2024-08-09 13:22:56 +0900

    Add files via upload

---
+++ 해시/폰켓몬/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {3,3,3,2,2,4};
+
+        Solution solution = new Solution();
+        solution.solution(nums);
+    }
+}
