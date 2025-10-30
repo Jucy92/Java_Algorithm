4581536c0e12b2a23fe01a5192bd80f21823a992
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2025-10-31 01:34:40 +0900

    최적화 코드...

---
+++ 프로그래머스/Lv.1/과일장수/Solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) throws InterruptedException {
+        int answer = 0;
+        int n = score.length;
+        Arrays.sort(score);
+
+        for (int i = n - m; i >= 0; i -= m) {
+            answer += score[i] * m;
+        }
+        return answer;
+    }
+}
