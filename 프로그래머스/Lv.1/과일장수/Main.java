23645e762e320758751deefdd73545091be62e56
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2025-10-30 23:55:18 +0900

    Add files via upload

---
+++ 프로그래머스/Lv.1/과일장수/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        Solution solution = new Solution();
+        SolutionV2 solution2 = new SolutionV2();
+
+        System.out.println(solution.solution(3,4, new int[]{1, 2, 3, 1, 2, 3, 1}));
+        System.out.println(solution.solution(4,3, new int[]{4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2}));
+
+
+
+    }
+}
