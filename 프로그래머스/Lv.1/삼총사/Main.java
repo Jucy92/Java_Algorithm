56d755d4c9035cbfbf56053a42d2a04ed1ca5708
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2025-11-08 17:10:59 +0900

    3 ≤ number의 길이 ≤ 13 (C(13,3) = 286)
    단순성: n ≤ 13으로 작아서 3중 for문이 가장 직관적
    재귀 오버헤드 없음: 함수 호출 비용 없음
    디버깅 용이: 로직이 한눈에 보임

---
+++ 프로그래머스/Lv.1/삼총사/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        SolutionV2 solution2 = new SolutionV2();
+
+
+        System.out.println(solution.solution(new int[]{-2, 3, 0, 2, -5}));
+        System.out.println(solution.solution(new int[]{-3, -2, -1, 0, 1, 2, 3}));
+        System.out.println(solution.solution(new int[]{-1, 1, -1, 1}));
+
+
+    }
+}
