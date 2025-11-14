f74a2e36dbf2aafd27e639a880bd03c04ea27c4d
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2025-11-14 10:54:18 +0900

    Add files via upload

---
+++ 프로그래머스/Lv.1/없는 숫자 더하기(월간 코드 챌린지 시즌3)/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        SolutionV2 solution2 = new SolutionV2();
+
+        System.out.println(solution2.solution(new int[]{1, 2, 3, 4, 6, 7, 8, 0}));
+        System.out.println(solution2.solution(new int[]{5,8,4,0,6,7,9}));
+
+
+//        System.out.println(solution.solution(new String[]{"TR", "RT", "TR"}, new int[]{7, 1, 3}));
+
+
+    }
+}
---
+++ 프로그래머스/Lv.1/없는 숫자 더하기(월간 코드 챌린지 시즌3)/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        boolean[] exists = new boolean[10];
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            exists[numbers[i]] = true;
+        }
+
+        for (int i = 0; i < exists.length; i++) {
+            if (!exists[i]) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+}
