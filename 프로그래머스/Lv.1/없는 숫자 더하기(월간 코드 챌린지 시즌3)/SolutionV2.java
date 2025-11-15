3422d0daf2de733cb0eef48c1e1e4b62ad70257b
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2025-11-15 20:14:42 +0900

    Add files via upload

---
+++ 프로그래머스/Lv.1/없는 숫자 더하기(월간 코드 챌린지 시즌3)/SolutionV2.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class SolutionV2 {
+    public int solution(int[] numbers) {
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return 45 - sum;
+    }
+}
