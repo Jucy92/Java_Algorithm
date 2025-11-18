6ca8ae6a3d2f4b2211692a8f427cbc4d05f1d12d
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2025-11-18 11:35:25 +0900

    Add files via upload

--- 프로그래머스/Lv.1/약수의 개수와 덧셈(월간 코드 챌린지 시즌2)/Solution.java
+++ 프로그래머스/Lv.1/약수의 개수와 덧셈(월간 코드 챌린지 시즌2)/Solution.java
@@ -4,7 +4,7 @@ class Solution {
 
         for (int i = left; i <= right; i++) {
             int sqrt = (int) Math.sqrt(i);
-            if (sqrt * sqrt == i) { // 완전제곱인 경우
+            if (sqrt * sqrt == i) { // 완전제곱인 경우 반환 받은 값으로 그대로 곱하면 원래 숫자가 나옴 , 본인과 떨어지는 값이 있기 때문에 홀수
                 sum -= i;
             } else {
                 sum += i;
