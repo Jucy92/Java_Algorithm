7461334cc9dcb49b05b7fb166a3e77f654cf2a45
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2025-11-24 15:28:15 +0900

    Add files via upload

---
+++ 프로그래머스/Lv.1/신규 아이디 추천(2021 KAKAO BLIND RECRUITMENT)/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        SolutionV2 solution2 = new SolutionV2();
+
+        System.out.println(solution.solution("...!@BaT#*..y.abcdefghijklm"));
+        System.out.println(solution.solution("z-+.^."));
+        System.out.println(solution.solution("=.="));
+
+
+    }
+}
