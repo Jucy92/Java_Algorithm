7bea4db7a742315136b61b35739c7f43aae3c201
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2025-11-25 12:00:06 +0900

    Add files via upload

---
+++ 프로그래머스/Lv.1/내적(월간 코드 챌린지 시즌1)/Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        SolutionV2 solution2 = new SolutionV2();
+
+        System.out.println(solution.solution(new int[]{1,2,3,4}, new int[]{-3,-1,0,2}));
+//        System.out.println(solution2.solution("...!@BaT#*..y.abcdefghijklm"));
+//        System.out.println(solution2.solution("z-+.^."));
+//        System.out.println(solution2.solution("=.="));
+
+
+    }
+}
