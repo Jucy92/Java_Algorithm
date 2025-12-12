61ea2063e06bb7bde02da28518b40e4a85d387fc
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2025-12-12 12:10:15 +0900

    기존 V2 시간 초과로 최적화

--- 프로그래머스/Lv.1/예산(Summer,Winter Coding(~2018))/SolutionV2.java
+++ 프로그래머스/Lv.1/예산(Summer,Winter Coding(~2018))/SolutionV2.java
@@ -1,37 +1,26 @@
 import java.util.*;
 
 class SolutionV2 {
-    int[] d;
-    int budget;
-    int count;
-    boolean[] visited;
+    int maxCount = 0;
+
     public int solution(int[] d, int budget) {
-        count = 0;
-        int answer = 0;
-        int sum =0;
-        this.d = d;
-        this.budget = budget;
-        this.visited = new boolean[d.length];
+        Arrays.sort(d);
+        dfs(d,0, 0, 0, budget);
 
-        dfs(0,0);
-
-        return count;
+        return maxCount;
     }
 
-    private void dfs(int num, int depth) {
-//        System.out.println("num = " + num + ", depth = " + depth);
-        if (num > budget) {
-            return;
-        }
-        count = Math.max(count, depth);
-        for (int i = 0; i < d.length; i++) {
-//            System.out.println("visited["+i+"] = " + visited[i]);
-            if (!visited[i]) {
-                visited[i] = true;
-                dfs(num+d[i], depth+1);
-                visited[i] = false;
-            }
-        }
+    private void dfs(int[] d, int index, int sum, int count, int budget) {
+        maxCount = Math.max(maxCount, count);  // sum 값을 넘겨주고 비교하는게 아니라 가능 할 때만 넘겨주기 때문에, 호출되고 바로 처리해도 괜찮.. => 내가 코드를 어떻게 짜냐에 따라 다르지..
 
+        if (budget < sum) return;
+
+        if (index == d.length) return;
+
+        if (sum + d[index] <= budget) {
+            dfs(d, index+1, sum+d[index], count+1, budget);
+        }
+//        dfs(d, index+1, sum, count, budget);  // 배열 정리 안했으면 필요하지만, 배열을 정렬 했기 때문에 위에서 통과가 안되면 뒤로는 다 budget보다 크기 떄문에 의미 없음..
     }
+
 }
