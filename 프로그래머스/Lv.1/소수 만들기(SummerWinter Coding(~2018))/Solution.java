9f647ae05b2868f8d1f35cf5b156c4a72817f9ea
Jucy92 <97105334+Jucy92@users.noreply.github.com>
2025-12-15 17:40:38 +0900

    Add files via upload

---
+++ 프로그래머스/Lv.1/소수 만들기(SummerWinter Coding(~2018))/Solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+
+
+    public int solution(int[] nums) {
+        int count = 0;
+
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (isDecimal(sum)) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private boolean isDecimal(int num) {
+        if (num < 2) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+        for (int i = 3; i*i <= num; i+=2) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+}
